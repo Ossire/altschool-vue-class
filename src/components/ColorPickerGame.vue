9205f3485c648b73cc1a5848fe2d779b4bc27a07
Kingsley Sado <kinglogomhe@gmail.com>
2023-02-07 23:47:08 +0100

    added color-picker game logic as a composable

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <h1>Color Picker Game</h1>
 
@@ -10,27 +12,37 @@
 
 <script>
 import { ref, reactive } from "@vue/reactivity";
+import colorEngine from "./composables/colorengine.js"
 
 export default {
+  
   setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
+    const {colors, message, matchColor} = colorEngine()
 
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
-    return { colors, message, matchColor };
+     return { colors, message, matchColor }
   },
+
+ 
 };
+
+
+
+
 </script>
 
 <style></style>
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+altschool-class-of-vue/ColorPickerGame.vue at master · iykeevans/altschool-class-of-vue · GitHub
